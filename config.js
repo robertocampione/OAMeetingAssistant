d643e2b6ca72e9b63a3596a2aa7ba4ec77b9cd1d
robertocampione <roberto.campione@gmail.com>
2025-05-01 10:34:49 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // config.js
 window.appConfig = {
   // Cambia qui l’URL in base all’ambiente (DEV, TEST, PROD)
-  endpointFlow1Url: "https://prod-57.westeurope.logic.azure.com:443/workflows/6cdadad69c1046209699010beb385db7/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=LLgMb2AWJ5EPsCzpRGzocvt26pIFHRbd_3ZiDqydt_M",
-  endpointFlow2Url: "https://prod-79.westeurope.logic.azure.com:443/workflows/2efe7307d7314cdf9090100f849a2907/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=aIWX0udWPgGjpKJQHHW59okVnNb8iLU2_W_MFQYRrNs", 
-  endpointFlow3Url: "https://prod-76.westeurope.logic.azure.com:443/workflows/7f4f9a0661a44f2bafa0b29026c85ad4/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=b5C7-5eFmoNhLWidbfGzcFf8o8Rfq-mnqZPxOvUFUyo"
+  endpointFlow1Url: "https://xxx/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=LLgMb2AWJ5EPsCzpRGzocvt26pIFHRbd_3ZiDqydt_M",
+  endpointFlow2Url: "https://yyy/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=aIWX0udWPgGjpKJQHHW59okVnNb8iLU2_W_MFQYRrNs", 
+  endpointFlow3Url: "https://zzz/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=b5C7-5eFmoNhLWidbfGzcFf8o8Rfq-mnqZPxOvUFUyo"
 };
